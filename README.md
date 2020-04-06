9629efd376901dfb6902bf8248532d720faea6b8
Amjad Ali <amjad.4@gmail.com>
2020-04-07 00:28:46 +0530

    Fix pending issues, clean up a little

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # HN Saved Links Export
 
-This is a python script to export your saved links from [Hacker News](https://news.ycombinator.com/news). What's a saved link, you ask? Any story that you upvote on HN is added to your saved links. You can access them from `https://news.ycombinator.com/saved?id=<username>` where `<username>` is your HN username.
+This is a python script to export your saved links from [Hacker News](https://news.ycombinator.com/news). What's a saved link, you ask? Any story that you upvote on HN is added to your saved links. You can access them at `https://news.ycombinator.com/upvoted?id=<your username>` while logged in.
 
 ### Instructions
-1. This script depends on `lxml` and `requests` packages, which should be installed by default on most systems. If not, install them like so:
- ```bash
- $ pip install -r requirements.txt
- ```
- You might have to use `sudo` with that command on Linux.
+Python 3 is required to run this. If you don't have it installed, install it using the package manager for your OS before following these instructions.
+
+1. Setup a virtual env (recommended, but optional) and install the dependencies:
+    ```bash
+    # Set up and activate a Python 3 virtual env
+    $ python3 -m venv venv
+    $ source venv/bin/activate
+
+    # Install dependencies
+    $ pip install -r requirements.txt
+    ```
+    You might have to use `sudo` with the last command on Linux.
 
 2. Run the script and enter your HN login credentials.
- ```bash
- $ python export_links.py
- Enter your HN account details:
- Username: amjd
- Password: [password will be hidden]
- ```
+    ```bash
+    $ python3 export_links.py
+    Enter your HN account details:
+    Username: amjd
+    Password: [password will be hidden]
+    ```
 
 3. After all the links are processed, you will be asked to enter an output file name. Use any name with the  extension `.json` or `.csv`, or leave blank to go with the default `links.json`. The output file will be saved in the appropriate format.
 
@@ -69,16 +76,11 @@ JSON:
 ```
 
 ### Todo
-- Add CLI options
-
+- Add support for exporting favourites.
+- Convert this into a CLI.
 
 ### Similar
 [HN Saved Export](https://github.com/thomaskcr/hn-saved-export) by thomaskcr
 
 ### Contribute
 If you think of any improvements, create an issue or send a pull request. :)
-
-### Python3
-
-Adjusted, python3 compatible.
-
--- export_links.py
+++ export_links.py
@@ -10,31 +10,41 @@ import sys
 from getpass import getpass
 from lxml import html
 
-BASE_URL = "https://news.ycombinator.com/"
+BASE_URL = "https://news.ycombinator.com"
+
 
 def signal_handler(signal, frame):
-    print('\nExiting...')
-    sys.exit(0)
+    print("\nStopping...\n")
+    sys.exit(130)
+
 
 def save_json(saved_links, file_name):
-    result = {
-        "total": len(saved_links),
-        "links": saved_links
-    }
-    with open(file_name, 'w') as f:
+    result = {"total": len(saved_links), "links": saved_links}
+    with open(file_name, "w") as f:
         json.dump(result, f)
 
-    print("Links saved to: {}".format(file_name))
+    print(f"\nLinks saved to: {file_name}")
 
 
 def save_csv(saved_links, file_name):
-    data = [(link["number"], link["title"], link["url"], link["points"], link["comments"],
-             link["comments_url"], link["author"], link["age"]) for link in saved_links]
-    with open(file_name, 'w') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, delimiter=',')
+    data = [
+        (
+            link["number"],
+            link["title"],
+            link["url"],
+            link["points"],
+            link["comments"],
+            link["comments_url"],
+            link["author"],
+            link["age"],
+        )
+        for link in saved_links
+    ]
+    with open(file_name, "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, delimiter=",")
         writer.writerows(data)
 
-    print("Links saved to: {}".format(file_name))
+    print(f"\nLinks saved to: {file_name}")
 
 
 def main():
@@ -47,22 +57,21 @@ def main():
     password = getpass()
 
     try:
-        print("Logging in...")
+        print("\nLogging in...")
 
-        r = session.post(BASE_URL + "login", data={"acct": username, "pw": password})
+        r = session.post(f"{BASE_URL}/login", data={"acct": username, "pw": password})
 
         if session.cookies.get("user", None) is None:
             print("Error logging in. Verify the credentials and try again.")
             sys.exit(1)
-        print("Logged in successfully.")
+        print("Logged in successfully.\n")
     except:
-        print("Error logging in.")
+        print("Error logging in.\n")
         sys.exit(1)
 
-#   url = "{}saved?id={}&p=".format(BASE_URL, username)
-    url = "{}upvoted?id={}&p=".format(BASE_URL, username)
+    url = f"{BASE_URL}/upvoted?id={username}&p="
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:75.0) Gecko/20100101 Firefox/75.0",
     }
 
     saved_links = list()
@@ -71,7 +80,7 @@ def main():
 
     while True:
         try:
-            print("Get url: ", url + str(i))
+            print("Getting url: ", url + str(i))
             r = session.get(url + str(i), headers=headers)
 
             tree = html.fromstring(r.text)
@@ -89,10 +98,10 @@ def main():
             n = len(tree_score)
 
             if n == 0:
-                print("Processing page {}. No links found.".format(i))
+                print(f"Processing page {i}. No links found.")
                 break
 
-            print("Processing page {}. Number of links found: {}".format(i, n))
+            print(f"Processing page {i}. Number of links found: {n}")
 
             for j in range(n):
                 tree_subtext_each = tree_subtext[j].cssselect("a")
@@ -100,25 +109,34 @@ def main():
 
                 link_dict = {
                     "number": int(tree_title[2 * j].text_content()[:-1]),
-                    "title": tree_title_each[0].text_content().encode("utf-8"),
+                    "title": tree_title_each[0].text_content(),
                     "url": tree_title_each[0].values()[0],
                     "points": int(tree_score[j].text_content().split()[0]),
                     "author": tree_subtext_each[0].text_content(),
                     "age": tree_subtext_each[1].text_content(),
                 }
 
+                # Use correct URL for stories without links (e.g., Ask HN, Show HN, etc.)
+                if link_dict["url"].startswith("item?"):
+                    link_dict["url"] = f"{BASE_URL}/{link_dict['url']}"
+
                 # This is to take care of situations where flag link may not be
                 # present in the subtext. So number of links could be either 3
                 # or 4.
                 num_subtext = len(tree_subtext_each)
-                link_dict["comments_url"] = BASE_URL + \
-                    tree_subtext_each[num_subtext - 1].values()[0]
+                link_dict["comments_url"] = (
+                    BASE_URL + tree_subtext_each[num_subtext - 1].values()[0]
+                )
 
-                if tree_subtext_each[num_subtext - 1].text_content().strip() == "discuss":
+                if (
+                    tree_subtext_each[num_subtext - 1].text_content().strip()
+                    == "discuss"
+                ):
                     link_dict["comments"] = 0
                 else:
                     link_dict["comments"] = int(
-                        tree_subtext_each[num_subtext - 1].text_content().split()[0])
+                        tree_subtext_each[num_subtext - 1].text_content().split()[0]
+                    )
 
                 saved_links.append(link_dict)
                 links_processed += 1
@@ -126,18 +144,22 @@ def main():
             if n < 30:
                 break
         except:
-            print("Error getting data for page {}".format(i))
+            print(f"Error getting data for page {i}")
             break
 
         i += 1
 
     if links_processed < 1:
-        print("Could not retrieve any of the links. Check if you actually have any saved links.")
+        print(
+            "Could not retrieve any of the links. Check if you actually have any saved links."
+        )
         sys.exit(1)
     else:
-        print("Processed {} links".format(links_processed))
+        print(f"Processed {links_processed} links")
 
-    print("Enter the file name in the next line. Use extension '.json' for JSON, or '.csv' for CSV.")
+    print(
+        "\nEnter the file name in the next line. Use extension '.json' for JSON, or '.csv' for CSV."
+    )
     file_name = input("File name (default: links.json): ")
 
     if file_name == "":
@@ -148,5 +170,6 @@ def main():
     elif file_name.split(".")[-1].lower() == "csv":
         save_csv(saved_links, file_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
